0fec72237efc84a77fb9ef77efaf64cd053cd72b
ASEYEUASZ1234 <a.seye3777@zig.univ.sn>
2021-04-23 00:18:43 +0200

    cart-checkout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 use \App\Http\Livewire\HomeComponent;
+use \App\Http\Livewire\ShopComponent;
+use \App\Http\Livewire\CartComponent;
+use \App\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +21,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/',HomeComponent::class);
+Route::get('/shop',ShopComponent::class);
+Route::get('/cart',CartComponent::class);
+Route::get('/checkout',CheckoutComponent::class);
