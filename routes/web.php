5b8ee282b76b18e46d04a616b8cddb3336b688e6
ASEYEUASZ1234 <a.seye3777@zig.univ.sn>
2021-04-24 14:05:34 +0200

    laravel auth

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use \App\Http\Livewire\ShopComponent;
 use \App\Http\Livewire\CartComponent;
 use \App\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Livewire\User\UserDashboardComponent;
+use \App\Http\Livewire\Admin\AdminDashboardComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,17 @@ Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
 Route::get('/cart',CartComponent::class);
 Route::get('/checkout',CheckoutComponent::class);
+
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//    return view('dashboard');
+//})->name('dashboard');
+
+
+Route::middleware(['auth:sanctum','verified'])->group(function(){
+     Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+});
+
+Route::middleware(['auth:sanctum','verified'])->group(function(){
+  Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
